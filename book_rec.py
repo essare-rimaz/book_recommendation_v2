f12febb73c95b9340a02e83e0ce8c821d7336132
essare-rimaz <richard.mazur9@gmail.com>
2024-10-13 12:19:21 +0200

    refactor: remove for loop
    there was only one element in it anyway and at any point we would pop only one book - the book based on which we recommend.
    
    in addition the variable was called "trilogy" - we would actually want to recommend continuation of books (not to mention it would be difficult to group books based on whether they do or do not belong in a saga). E.g. I want to be recommended the two towers if I liked fellowship.

--- book_rec.py
+++ book_rec.py
@@ -36,39 +36,36 @@ ratings_data_raw_nodup = ratings_data_raw.groupby(['User-ID', 'Book-Title'])['Bo
 ratings_data_raw_nodup = ratings_data_raw_nodup.to_frame().reset_index()
 
 dataset_for_corr = ratings_data_raw_nodup.pivot(index='User-ID', columns='Book-Title', values='Book-Rating')
-
-LoR_list = ['the fellowship of the ring (the lord of the rings, part 1)']
+LoR_book = 'the fellowship of the ring (the lord of the rings, part 1)'
 
 result_list = []
 worst_list = []
 
-# for each of the trilogy book compute:
-for LoR_book in LoR_list:
-    
-    #Take out the Lord of the Rings selected book from correlation dataframe
-    dataset_of_other_books = dataset_for_corr.copy(deep=False)
-    dataset_of_other_books.drop([LoR_book], axis=1, inplace=True)
-      
-    # empty lists
-    book_titles = []
-    correlations = []
-    avgrating = []
 
-    # corr computation
-    for book_title in list(dataset_of_other_books.columns.values):
-        book_titles.append(book_title)
-        correlations.append(dataset_for_corr[LoR_book].corr(dataset_of_other_books[book_title]))
-        tab=(ratings_data_raw[ratings_data_raw['Book-Title']==book_title].groupby(ratings_data_raw['Book-Title']).mean())
-        avgrating.append(tab['Book-Rating'].min())
-    # final dataframe of all correlation of each book   
-    corr_fellowship = pd.DataFrame(list(zip(book_titles, correlations, avgrating)), columns=['book','corr','avg_rating'])
-    corr_fellowship.head()
-
-    # top 10 books with highest corr
-    result_list.append(corr_fellowship.sort_values('corr', ascending = False).head(10))
+#Take out the Lord of the Rings selected book from correlation dataframe
+dataset_of_other_books = dataset_for_corr.copy(deep=False)
+dataset_of_other_books.drop([LoR_book], axis=1, inplace=True)
     
-    #worst 10 books
-    worst_list.append(corr_fellowship.sort_values('corr', ascending = False).tail(10))
+# empty lists
+book_titles = []
+correlations = []
+avgrating = []
+
+# corr computation
+for book_title in list(dataset_of_other_books.columns.values):
+    book_titles.append(book_title)
+    correlations.append(dataset_for_corr[LoR_book].corr(dataset_of_other_books[book_title]))
+    tab=(ratings_data_raw[ratings_data_raw['Book-Title']==book_title].groupby(ratings_data_raw['Book-Title']).mean())
+    avgrating.append(tab['Book-Rating'].min())
+# final dataframe of all correlation of each book   
+corr_fellowship = pd.DataFrame(list(zip(book_titles, correlations, avgrating)), columns=['book','corr','avg_rating'])
+corr_fellowship.head()
+
+# top 10 books with highest corr
+result_list.append(corr_fellowship.sort_values('corr', ascending = False).head(10))
+
+#worst 10 books
+worst_list.append(corr_fellowship.sort_values('corr', ascending = False).tail(10))
     
 print("Correlation for book:", LoR_list[0])
 #print("Average rating of LOR:", ratings_data_raw[ratings_data_raw['Book-Title']=='the fellowship of the ring (the lord of the rings, part 1'].groupby(ratings_data_raw['Book-Title']).mean()))
